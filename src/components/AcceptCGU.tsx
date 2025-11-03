1d149c2c1e772774a87718cab8b4c840ffd1b008
Gauthier Amano <gauthier.net@gmail.com>
2025-11-03 14:31:10 +0100

    refactor(AcceptCGU): ♻️ use meaningful names + enforce conventions

--- src/components/AcceptCGU.tsx
+++ src/components/AcceptCGU.tsx
@@ -2,13 +2,14 @@ import { useState } from "react";
 import type { PropsLabel } from "../utils/types";
 
 export default function AcceptCGU({ label }: PropsLabel) {
-  const [checked, setChecked] = useState(false);
+  const [isCguAccepted, setIsCguAccepted] = useState(false);
+
   return (
     <label className="flex items-center gap-2">
       <input
         type="checkbox"
-        checked={checked}
-        onChange={(e) => setChecked(e.target.checked)}
+        checked={isCguAccepted}
+        onChange={(event) => setIsCguAccepted(event.target.checked)}
       />
       {label}
     </label>
